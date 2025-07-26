7742124a195a6cb52247f5c36950c2e1ae8b9309
SolisysDigital <solisys@solisyspro.com>
2025-07-26 18:08:30 -0500

    UI: Update table text colors for improved readability

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -87,7 +87,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle bg-white text-black [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("p-4 align-middle bg-white text-teal-800 [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ))
