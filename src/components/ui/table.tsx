ea1675d60674ae1f66703b4682946390b0a7bdee
SolisysDigital <solisys@solisyspro.com>
2025-07-26 17:41:49 -0500

    UI: Update table styles to match professional design with light gray header and clean row separators

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,7 +20,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("bg-gray-200", className)} {...props} />
+  <thead ref={ref} className={cn("bg-gray-300", className)} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
@@ -30,7 +30,7 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr]:border-b", className)}
+    className={cn("bg-white", className)}
     {...props}
   />
 ))
@@ -43,7 +43,7 @@ const TableFooter = React.forwardRef<
   <tfoot
     ref={ref}
     className={cn(
-      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
+      "border-t bg-white font-medium [&>tr]:last:border-b-0",
       className
     )}
     {...props}
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b border-gray-300 transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b border-gray-200 bg-white transition-colors hover:bg-gray-50 data-[state=selected]:bg-muted",
       className
     )}
     {...props}
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-bold text-gray-800 border-b bg-gray-200 uppercase text-xs [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-bold text-white bg-gray-300 uppercase text-xs [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
@@ -87,7 +87,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle border-b border-gray-100 [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("p-4 align-middle bg-white text-black [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ))
