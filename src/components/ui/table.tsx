f526cd42c97c0e20123da87c657a6d1fa3a75590
SolisysDigital <solisys@solisyspro.com>
2025-08-03 20:19:40 -0500

    Update table header background to bg-slate-500 with white text for better contrast

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,7 +20,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b bg-slate-200", className)} {...props} />
+  <thead ref={ref} className={cn("[&_tr]:border-b bg-slate-500", className)} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-semibold text-muted-foreground bg-slate-200 [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-semibold text-white bg-slate-500 [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
